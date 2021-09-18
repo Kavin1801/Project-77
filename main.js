e547ab4e2597d60842abbce9a0dcf38949ae501f
Kavin1801 <84767706+Kavin1801@users.noreply.github.com>
2021-09-18 11:10:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var f_name= "Kavin ";
+var l_name= "Acharya";
+
+console.log(f_name.concat(l_name));
+
+var a = 10;
+var b = 5;
+
+var add= a + b;
+console.log(add);
+
+var subtract= a - b;
+console.log(subtract);
+
+var multiply= a * b;
+console.log(multiply);
+
+var divide= a / b;
+console.log(divide);
